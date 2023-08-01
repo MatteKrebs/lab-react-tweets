5bd0c7df35d16f26046c8a4bd44f7761f766e7ec
Matt Krebs <mattekrebs@gmail.com>
2023-08-01 09:37:51 +0200

    Almost

---
+++ src/components/ProfileImage.js
@@ -0,0 +1,11 @@
+function ProfileImage(props) {
+    return (
+        <img
+        src={props.userImage}
+        className="profile"
+        alt="profile"
+      />
+    )
+}
+
+export default ProfileImage;
